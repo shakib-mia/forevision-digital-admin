bd77e4453fc41294c316438ef725fe563d03d9ca
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-12-27 19:27:24 +0600

    backend url is changed

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const backendUrl = "https://forevision-digital.onrender.com/";
-// export const backendUrl = "http://localhost:5000/";
+// export const backendUrl = "https://forevision-digital.onrender.com/";
+export const backendUrl = "http://193.203.162.180:5000/";
